22fcc1b7e50e150a50d45b03671e579420bffe4f
Christie Yoon <cleex1999@gmail.com>
2020-08-06 14:51:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies][:languages.value]
-     programmer_hash
+		 programmer_hash[:dennis_ritchies][:languages]
+     #programmer_hash
 end
 
 def adding_matz
